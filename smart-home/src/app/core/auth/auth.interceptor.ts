54d6bdc433aa946de9a6af15327af16ca0513d6e
Anna Golosova <a.golosova@3l.ru>
2025-07-28 19:09:37 +0300

    feat: add interceptor

--- smart-home/src/app/core/auth/auth.interceptor.ts
+++ smart-home/src/app/core/auth/auth.interceptor.ts
@@ -1,5 +1,33 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
+import {inject} from '@angular/core';
+import {TokenService} from '@/app/core/auth/services/token/token.service';
+import {Router} from '@angular/router';
+import {catchError, throwError} from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const tokenService = inject(TokenService);
+  const router = inject(Router);
+
+  let modifiedReq = req;
+
+  //добавление token к headers
+  const token = tokenService.getToken();
+  if (token) {
+    modifiedReq = modifiedReq.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+
+  // обработка  error 401
+  return next(modifiedReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        tokenService.clearToken();
+        router.navigate(['/login']).catch(() => {});
+      }
+      return throwError(() => error);
+    })
+  );
 };
