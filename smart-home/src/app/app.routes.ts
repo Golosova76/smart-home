4ba1f8fe2a6ed248d449ffa35cc11611ba31bb23
Anna Golosova <a.golosova@3l.ru>
2025-07-28 18:03:24 +0300

    feat: add login-page

--- smart-home/src/app/app.routes.ts
+++ smart-home/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {LoginPageComponent} from '@/app/core/auth/login-page/login-page.component';
 import {NotFoundPageComponent} from '@/app/core/not-found-page/not-found-page.component';
 import {NavigationComponent} from '@/app/core/navigation/navigation.component';
+
 import {DashboardComponent} from '@/app/core/dashboard/dashboard.component';
 
 export const routes: Routes = [
--- smart-home/src/app/core/auth/login-page/login-page.component.ts
+++ smart-home/src/app/core/auth/login-page/login-page.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '@/app/core/auth/services/auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  isPasswordVisible = signal<boolean>(false)
+
+  form: FormGroup<{
+    username: FormControl<string | null>;
+    password: FormControl<string | null>;
+  }> = new FormGroup({
+    username: new FormControl<string | null>(null, Validators.required),
+    password: new FormControl<string | null>(null, Validators.required),
+  });
+
+  togglePasswordVisible(): void {
+    this.isPasswordVisible.set(!this.isPasswordVisible());
+  }
+
+
+  onSubmit() {
+    if (this.form.valid) {
+      const formData = {
+        userName: this.form.value.username ?? '',
+        password: this.form.value.password ?? '',
+      }
+      this.authService.login(formData).subscribe(async () => {
+        await this.router.navigate(['']);
+      })
+    }
+  }
 
 }
