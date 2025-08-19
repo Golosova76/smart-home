f2910bae1df172e60a13b62b2788eb2353b105fd
Anna Golosova <a.golosova@3l.ru>
2025-08-19 17:42:12 +0300

    feat: add NgRx

--- smart-home/src/app/app.config.ts
+++ smart-home/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
+  provideZoneChangeDetection, isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -12,6 +12,9 @@ import {
   withInterceptors,
 } from '@angular/common/http';
 import { authInterceptor } from '@/app/core/auth/auth.interceptor';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,5 +22,8 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
-  ],
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+],
 };
