59c75e01fbc14ed0daca3dd77ecdf0487a406168
Anna Golosova <a.golosova@3l.ru>
2025-08-20 18:02:29 +0300

    feat: add actions

---
+++ smart-home/src/app/store/actions/dashboard.actions.ts
@@ -0,0 +1,17 @@
+import {createAction} from "@ngrx/store";
+
+export const enterEditMode = createAction(
+    "[Dashboard] Enter Edit Mode"
+);
+
+export const exitEditMode = createAction(
+    "[Dashboard] Exit Edit Mode"
+);
+
+export const saveDashboard = createAction(
+    "[Dashboard] Save Dashboard"
+);
+
+export const discardChanges = createAction(
+    "[Dashboard] Discard Changes"
+);
